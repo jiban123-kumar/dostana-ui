77cf68c770f11db684da7a2338d3c75cfa5e81b5
jiban123-kumar <jibandon12345@gmail.com>
2025-03-07 21:21:37 +0530

    final

--- src/main.jsx
+++ src/main.jsx
@@ -52,13 +52,3 @@ createRoot(document.getElementById("root")).render(
     </Provider>
   </StrictMode>
 );
-if ("serviceWorker" in navigator) {
-  navigator.serviceWorker
-    .register("/firebase-messaging-sw.js")
-    .then((registration) => {
-      console.log("Service Worker registered with scope:", registration.scope);
-    })
-    .catch((error) => {
-      console.error("Service Worker registration failed:", error);
-    });
-}
